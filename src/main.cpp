667cb3eec04565993da9f741ca956b9af8916703
Alexi <alexi.bara@hotmail.com>
2018-01-24 21:42:52 +0100

    test Phyvec

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,19 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    Phyvec *p = new Phyvec();
+    Phyvec *p = new Phyvec(2);
 
     p->setX(21);
     p->setY(42);
     p->setZ(84);
+    cout << "Dimension: " << p->getDim() << endl;
     cout << *p << endl;
+    cout << "Z: " << p->getZ() << endl;
+
+    p->setDim(3);
+    p->setZ(84);
+    cout << "Dimension: " << p->getDim() << endl;
+    cout << *p << endl;
+    cout << "Z: " << p->getZ() << endl;
     return 0;
 }
